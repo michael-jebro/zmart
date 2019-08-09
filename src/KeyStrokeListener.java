5e2ce5bd1edf7a5e972dec929c99ef0efb00ee9e
Michael <wanderer.x64@gmail.com>
2019-08-09 16:29:30 +0300

    project architecture changed

--- src/KeyStrokeListener.java
+++ src/KeyStrokeListener.java
@@ -1,27 +1,53 @@
-import com.googlecode.lanterna.terminal.*;
-import com.googlecode.lanterna.bundle.*;
-import com.googlecode.lanterna.graphics.*;
 import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.input.*;
-import com.googlecode.lanterna.screen.*;
 import com.googlecode.lanterna.*;
+import scene.Root;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class KeyStrokeListener implements WindowListener {
-    public void onInput(Window basePane, KeyStroke keyStroke, AtomicBoolean deliverEvent) {
-        System.out.println("Pressed: "+ keyStroke);
+public class KeyStrokeListener /*implements WindowListener*/ {
+
+    /*public void onInput(Window basePane, KeyStroke keyStroke, AtomicBoolean deliverEvent) {
+
+        if(keyStroke.getCharacter()!=null) {
+
+            if (keyStroke.isAltDown() & keyStroke.getCharacter() == 's') {
+                Root.text = Root.textPanel.getText();
+                Root.clearSurface();
+                Root.saveFileScene();
+                //Main.textGUI.removeWindow(Main.window);
+            }
+
+            if (keyStroke.isAltDown() & keyStroke.getCharacter() == 'r') {
+                Root.clearSurface();
+                Root.editFileScene();
+                //Main.textGUI.removeWindow(Main.window);
+            }
+
+        }
+
+        Main.window.setPosition(new TerminalPosition(10,10));
+        Main.window.setSize(new TerminalSize(10,10));
+        Main.textPanel.setSize(new TerminalSize(100,100));
+
+
+
     }
 
     public void onUnhandledInput(Window basePane, KeyStroke keyStroke, AtomicBoolean hasBeenHandled) {
-        // TODO Auto-generated method stub
+
     }
 
     public void onResized(Window window, TerminalSize oldSize, TerminalSize newSize) {
-        // TODO Auto-generated method stub
+        System.out.println(Main.screen.getTerminalSize()+" "+Main.window.getSize());
+        if(!Main.window.getSize().equals(Main.screen.getTerminalSize())) {
+            Main.window.setSize(Main.screen.getTerminalSize());
+            Main.contentPanel.setLayoutManager(new GridLayout());
+        }
     }
 
     public void onMoved(Window window, TerminalPosition oldPosition, TerminalPosition newPosition) {
-        // TODO Auto-generated method stub
-    }
+
+    }*/
+
 }
